d2e153c8f614890c26d5ec2daab35f15f12b6bb1
Bubu52 <edoguillem@gmail.com>
2024-01-18 12:53:31 +0100

    Modified

--- DATASETS/conexion_sql.py
+++ DATASETS/conexion_sql.py
@@ -1,19 +1,58 @@
 import pymysql
-
-# Funcion para conectar a la base de datos (MySQL)
+import pandas as pd
 # Local Host = 127.0.0.1
-def conectarBD():
+
+# Funcion para CONECTAR a la base de datos (MySQL)
+def conectarBD(host, user, password, db):
     try:
-        conexion = pymysql.connect(host="localhost", user="IMPELIA", passwd="<1234>", db="ml-tests")
-        return conexion
-    except:
-        print("Error al conectar a la base de datos")
-        exit()
+        conn = pymysql.connect(host=host, user=user, password=password, db=db)
+        return conn
+    except Exception as e:
+        print(e)
+        return False
+# Función para EJECUTAR una query para la base de datos
+
+def ejecutarQuery(connector, query):
+    try:
+        cur = connector.cursor()
+        cur.execute(query)
+        output = cur.fetchall()
+        return output
+    except Exception as e:
+        print(e)
+        return False
+    
+# Función para CERRAR la conexión a la base de datos
+def cerrarBD(conector):
+    try:
+        conector.close()
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
 # Gestión inicio codigo
 if __name__ == "__main__":
-    conexion = conectarBD()
-    cursor = conexion.cursor()
-    cursor.execute("CREATE DATABASE prueba")
-    conexion.commit()
-    conexion.close()
+
+    # Conectar
+    conexion = conectarBD("127.0.0.1", "IMPELIA", "1234", "ml-tests")
+    if conexion:
+        print(" [+] Conectado a la base de datos")
+
+        # Extraer todos los datos de la base de datos
+        data = ejecutarQuery(conexion, "SELECT * FROM restaurant_revenue")
+        if data:
+            # print(data) - Este print sirve para verificar que hay datos en la base de datos al principio.
+            df = pd.DataFrame(data)
+            print(df.head(10))
+        else:
+            print(" [-] Error de ejecución")
+
+        # Desconectar
+        close = cerrarBD(conexion)
+        if close:
+            print(" [+] Desconectados de la base de datos")
+        else:
+            print(" [-] Error al desconectar la conexión a la base de datos")
+    else:
+        print(" [-] Error al conectar a la base de datos")
